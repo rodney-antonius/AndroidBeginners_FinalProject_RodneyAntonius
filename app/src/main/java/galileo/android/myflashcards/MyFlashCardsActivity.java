3acd9af8a699fd43dfa0e00789dc647afcc4c9b6
Ródney <rodney.antonius@outlook.com>
2017-04-10 17:06:10 -0300

    Initial commit

---
+++ app/src/main/java/galileo/android/myflashcards/MyFlashCardsActivity.java
@@ -0,0 +1,13 @@
+package galileo.android.myflashcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFlashCardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_flash_cards);
+    }
+}

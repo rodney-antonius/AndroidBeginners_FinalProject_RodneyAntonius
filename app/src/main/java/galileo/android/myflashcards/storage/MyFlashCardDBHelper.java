3f3f2e1fa8533cc6009bbcb791b11316cf822d33
Ródney <rodney.antonius@outlook.com>
2017-04-10 19:00:36 -0300

    Content Provider Implemented

---
+++ app/src/main/java/galileo/android/myflashcards/storage/MyFlashCardDBHelper.java
@@ -0,0 +1,44 @@
+package galileo.android.myflashcards.storage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import galileo.android.myflashcards.storage.MyFlashCardsContract.FlashCardEntry;
+
+/**
+ * Created by Agro on 10/04/2017.
+ */
+
+public class MyFlashCardDBHelper extends SQLiteOpenHelper {
+
+    // If you change the database schema, you must increment the database version.
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String DATABASE_NAME = "myflashcard.db";
+
+    public MyFlashCardDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create a table to hold to-do.  A to-do is a task that need to be completed.
+
+        final String SQL_CREATE_TODO_TABLE = "CREATE TABLE " + FlashCardEntry.TABLE_NAME + " (" +
+                FlashCardEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                FlashCardEntry.COLUMN_QUESTION + " TEXT NOT NULL, " +
+                FlashCardEntry.COLUMN_ANSWER + " TEXT NOT NULL, " +
+                "UNIQUE (" + FlashCardEntry.COLUMN_QUESTION+ ", " + FlashCardEntry.COLUMN_ANSWER + ") ON " +
+                "CONFLICT IGNORE" +
+                " );";
+
+        sqLiteDatabase.execSQL(SQL_CREATE_TODO_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + FlashCardEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/galileo/android/myflashcards/storage/MyFlashCardProvider.java
@@ -0,0 +1,192 @@
+package galileo.android.myflashcards.storage;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import galileo.android.myflashcards.storage.MyFlashCardsContract.FlashCardEntry;
+
+/**
+ * Created by Agro on 10/04/2017.
+ */
+
+public class MyFlashCardProvider extends ContentProvider {
+
+    private static final int FLASH_CARD = 100;
+    private static final int FLASH_CARD_ID = 101;
+
+    // The URI Matcher used by this content provider.
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MyFlashCardDBHelper mOpenHelper;
+
+    private static UriMatcher buildUriMatcher() {
+        // I know what you're thinking.  Why create a UriMatcher when you can use regular
+        // expressions instead?  Because you're not crazy, that's why.
+
+        // All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found.  The code passed into the constructor represents the code to return for the root
+        // URI.  It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MyFlashCardsContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+
+        matcher.addURI(authority, MyFlashCardsContract.PATH_FLASH_CARD, FLASH_CARD);
+        matcher.addURI(authority, MyFlashCardsContract.PATH_FLASH_CARD + "/#", FLASH_CARD_ID);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new MyFlashCardDBHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        // Here's the switch statement that, given a URI, will determine what kind of request it is,
+        // and query the database accordingly.
+        Cursor retCursor;
+        switch (sUriMatcher.match(uri)) {
+            // "to-do"
+            case FLASH_CARD: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        FlashCardEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+            // "tod-o by id/*"
+            case FLASH_CARD_ID: {
+                retCursor = mOpenHelper.getReadableDatabase().query(
+                        FlashCardEntry.TABLE_NAME,
+                        projection,
+                        FlashCardEntry._ID + " = '" + ContentUris.parseId(uri) + "'",
+                        null,
+                        null,
+                        null,
+                        sortOrder
+                );
+                break;
+            }
+
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        retCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return retCursor;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case FLASH_CARD:
+                return FlashCardEntry.CONTENT_TYPE;
+            case FLASH_CARD_ID:
+                return FlashCardEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case FLASH_CARD: {
+                long _id = db.insert(FlashCardEntry.TABLE_NAME, null, contentValues);
+                if ( _id > 0 )
+                    returnUri = FlashCardEntry.buildFlashCardUri(_id);
+                else
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        getContext().getContentResolver().notifyChange(uri, null);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        switch (match) {
+            case FLASH_CARD:
+                rowsDeleted = db.delete(
+                        FlashCardEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // Because a null deletes all rows
+        if (selection == null || rowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match) {
+            case FLASH_CARD:
+                rowsUpdated = db.update(FlashCardEntry.TABLE_NAME, values, selection,
+                        selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if (rowsUpdated != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case FLASH_CARD:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                    for (ContentValues value : values) {
+                        long _id = db.insert(FlashCardEntry.TABLE_NAME, null, value);
+                        if (_id != -1) {
+                            returnCount++;
+                        }
+                    }
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
+
+    }
+}
